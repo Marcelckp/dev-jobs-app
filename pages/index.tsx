3a01bda856d2d23715048e735f83728cf067e985
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-01-30 20:21:11 +0200

    Template repo set Up, with static props set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,70 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
 
-export default function Home() {
+interface Options {
+  method: string,
+  url: string,
+  params: { [key: string]: any}
+  headers: { [key: string]: any}
+}
+
+interface Props {
+  data: {}
+}
+
+var options:Options = {
+  method: 'GET',
+  url: 'https://awesome-indeed.p.rapidapi.com/indeed_jobs_detailed',
+  params: {search_query: 'Software', page: '1'},
+  headers: {
+    'x-rapidapi-host': 'awesome-indeed.p.rapidapi.com',
+    'x-rapidapi-key': '5de04b4153msh02581199aae4872p19499cjsn45ead7167025'
+  }
+};
+
+export default function Home({ data } :Props) {
+  console.log(data);
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className=' w-full h-screen'>
+      <Head>Dev Jobs</Head>
+      <header className="absolute top-0 flex h-36 w-full items-center justify-center bg-[#5964E0]">
+        <div className="flex w-4/5 justify-between text-5xl text-white font-medium">
+          <div>
+            <h1>dev Jobs</h1>
+          </div>
+          <div>
+            <h1>Light Mode // Dark Mode</h1>
+          </div>
+        </div>
+      </header>
+      <main className="flex justify-center text-4xl text-black">
+        <div className="flex w-4/5 flex-col">
+          
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
+      <footer className=" fixed bottom-0 flex h-32 w-full justify-center items-center bg-white">
+        <div className='flex space-x-5 text-2xl '>
+          <div className='border-4 border-[#5964e0] py-4 px-6 rounded-xl cursor-pointer hover:bg-[#5964e0] hover:text-white transition ease-in-out duration-300'>
+            <button className='font-medium'>Sign In</button>            
+          </div>
+          <div className='border-4 border-[#5964e0] py-4 px-6 rounded-xl cursor-pointer hover:bg-[#5964e0] hover:text-white transition ease-in-out duration-300'>
+            <button className='font-medium'>Sign Up</button>
+          </div>
+        </div>
       </footer>
     </div>
   )
+}
+
+
+export const getStaticProps:GetStaticProps = async() => {
+  const data = await axios.request(options).then(res => {
+    return res.data;
+  })
+  return {
+    props: {
+      data
+    }
+  }
 }
